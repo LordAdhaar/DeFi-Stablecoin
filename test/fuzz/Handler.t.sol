270280a3bc64c60acb872916c0758c8938d9ecd0
Adhaar Jain <adhaarjain@Adhaars-MacBook-Air.local>
2024-11-29 01:56:12 +0530

    add invariant test to check DSCEngine.sol:depositCollateral with random values

--- test/fuzz/Handler.t.sol
+++ test/fuzz/Handler.t.sol
@@ -1,1 +1,37 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.28;
 
+import {Test, console} from "../../lib/forge-std/src/Test.sol";
+import {DSCEngine} from "../../src/DSCEngine.sol";
+import {DecentralizedStableCoin} from "../../src/DecentralizedStableCoin.sol";
+import {ERC20Mock} from "../mocks/ERC20Mock.sol";
+
+contract Handler is Test {
+    DSCEngine public dscEngine;
+    DecentralizedStableCoin public decentralizedStableCoin;
+    uint256 MAX_DEPOSIT_SIZE = type(uint96).max;
+
+    constructor(DSCEngine _dscEngine, DecentralizedStableCoin _decentralizedStableCoin) {
+        dscEngine = _dscEngine;
+        decentralizedStableCoin = _decentralizedStableCoin;
+    }
+
+    function handlerDepositCollateral(uint256 randomNumber, uint256 tokenAmount) public {
+        tokenAmount = bound(tokenAmount, 1, MAX_DEPOSIT_SIZE);
+        address tokenAddress = getValidCollateralAddress(randomNumber);
+
+        ERC20Mock(tokenAddress).mint(address(this), tokenAmount);
+        ERC20Mock(tokenAddress).approve(address(dscEngine), tokenAmount);
+
+        dscEngine.depositCollateral(tokenAddress, tokenAmount);
+    }
+
+    function getValidCollateralAddress(uint256 randomNumber) public view returns (address) {
+        address[] memory tokenAddresses = dscEngine.getTokensAcceptedAsCollateral();
+
+        if (randomNumber % 2 == 0) {
+            return tokenAddresses[0];
+        }
+        return tokenAddresses[1];
+    }
+}
--- test/fuzz/OpenInvariantsTest.t.sol
+++ test/fuzz/OpenInvariantsTest.t.sol
@@ -1,1 +1,52 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.28;
 
+import {DSCEngine} from "../../src/DSCEngine.sol";
+import {DecentralizedStableCoin} from "../../src/DecentralizedStableCoin.sol";
+import {HelperConfig} from "../../script/HelperConfig.s.sol";
+import {DeployDSCScript} from "../../script/DeployDSCScript.s.sol";
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+import {Test, console} from "../../lib/forge-std/src/Test.sol";
+import {StdInvariant} from "forge-std/StdInvariant.sol";
+import {Handler} from "./Handler.t.sol";
+import {ERC20Mock} from "../mocks/ERC20Mock.sol";
+
+// Inavariant to be tested = DSC minted < CollateralAmountInUSD
+
+contract OpenInvariantsTest is Test {
+    DSCEngine public dscEngine;
+    DecentralizedStableCoin public decentralizedStableCoin;
+
+    HelperConfig.NetworkConfig public activeNetworkConfig;
+    DeployDSCScript public deployDSCScript;
+
+    Handler public handler;
+
+    address public wethTokenAddress;
+    address public wbtcTokenAddress;
+
+    function setUp() external {
+        deployDSCScript = new DeployDSCScript();
+        (decentralizedStableCoin, dscEngine, activeNetworkConfig) = deployDSCScript.run();
+
+        wethTokenAddress = activeNetworkConfig.wETH;
+        wbtcTokenAddress = activeNetworkConfig.wBTC;
+
+        handler = new Handler(dscEngine, decentralizedStableCoin);
+
+        targetContract(address(handler));
+    }
+
+    function invariant_mintedDscLessThanCollateralInUsd() public view {
+        uint256 totalMintedDsc = decentralizedStableCoin.totalSupply();
+
+        uint256 wethCollateralAmount = ERC20Mock(wethTokenAddress).balanceOf(address(dscEngine));
+        uint256 wbtcCollateralAmount = ERC20Mock(wbtcTokenAddress).balanceOf(address(dscEngine));
+
+        uint256 wethCollateralAmountInUsd = dscEngine.getUSDValueOfCollateral(wethTokenAddress, wethCollateralAmount);
+        uint256 wbtcCollateralAmountInUsd = dscEngine.getUSDValueOfCollateral(wbtcTokenAddress, wbtcCollateralAmount);
+
+        assert(totalMintedDsc <= wethCollateralAmountInUsd + wbtcCollateralAmountInUsd);
+    }
+}
